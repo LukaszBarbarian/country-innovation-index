ad9c44a6be2160152c03bc724fd9a874dc362666
LukaszSurowiec <122279205+LukaszSurowiec@users.noreply.github.com>
2025-07-12 07:23:30 +0200

    test run job

--- databricks/common/config.py
+++ databricks/common/config.py
@@ -1,5 +1,3 @@
-dbutils.widgets.text("bronze_path", "/mnt/bronze/raw_jsons/", "Bronze Raw JSON Path")
-dbutils.widgets.text("silver_path", "/mnt/silver/processed_data/", "Silver Delta Table Path")
+from bronze_ingestion.api_client.base import ApiClient
 
-BRONZE_PATH_CONTAINER = dbutils.widgets.get("bronze_path")
-SILVER_PATH_CONTAINER = dbutils.widgets.get("silver_path")
+print("I'm here")
