79097f9c325a0d1a7868ef3d23cd94c1fe007a0a
LukaszSurowiec <122279205+LukaszSurowiec@users.noreply.github.com>
2025-09-09 18:32:51 +0200

    Fix

--- infra/manifest_storage/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_storage_blob" "manifest" {
-  for_each = { for m in var.manifests : "${m.layer}-${m.env}" => m }
-
-  name                   = "${each.value.env}.manifest.json"
-  storage_account_name   = var.storage_account_name
-  storage_container_name = each.value.container
-  type                   = "Block"
-  source                 = abspath(each.value.path)
-}
