fa9480f78a84c78442cbdc951c11b79710db69a4
LukaszSurowiec <122279205+LukaszSurowiec@users.noreply.github.com>
2025-09-14 19:20:18 +0200

    Fix

--- function_app.py
+++ function_app.py
@@ -1,12 +1,11 @@
 
 import logging
 
-import traceback
+
 from typing import Any, Dict
-import uuid
+
 import azure.functions as func
 import azure.durable_functions as df
-from src.common.config.config_manager import ConfigManager
 
 from src.bronze.init import bronze_init
 
@@ -61,27 +60,3 @@ def ingest_orchestrator(context: df.DurableOrchestrationContext):
     logger.info(f"Bronze orchestration completed. Result: {orchestrator_result_dict.get('status', 'N/A')}")
    
     return orchestrator_result_dict
-
-
-@app.activity_trigger(input_name="input")
-async def run_ingestion_activity(input: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Activity function to execute the bronze layer data ingestion logic AND
-    publish an event to Event Grid.
-    """
-    config = ConfigManager()
-    
-    input_payload = input["input_payload"]
-
-    correlation_id = input_payload.get("correlation_id", str(uuid.uuid4()))
-    logger.info(f"Running ingestion for correlation ID: {correlation_id}")
-    
-    result_dict = {
-            "status": "FAILED",
-            "correlation_id": correlation_id,
-            "message": str(e),
-            "error_details": traceback.format_exc(),
-        }
-    return result_dict
-    
-    
