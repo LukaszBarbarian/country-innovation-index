13b9c1cf7adc31614a693d3bc5540362b3c89ad5
LukaszSurowiec <122279205+LukaszSurowiec@users.noreply.github.com>
2025-09-15 13:09:58 +0200

    Fix

--- function_app.py
+++ function_app.py
@@ -1,19 +1,17 @@
 import datetime
 import logging
-import json
-import os
 import traceback
 from typing import Any, Dict
 import uuid
 import azure.functions as func
 import azure.durable_functions as df
-#from src.bronze.contexts.bronze_parser import BronzeParser
+from src.bronze.contexts.bronze_parser import BronzeParser
 from src.common.azure_clients.event_grid_client_manager import EventGridNotifier
-#from src.common.factories.orchestrator_factory import OrchestratorFactory
+from src.common.factories.orchestrator_factory import OrchestratorFactory
 from src.common.config.config_manager import ConfigManager
 from src.common.enums.etl_layers import ETLLayer
 
-#from src.bronze.init import bronze_init
+from src.bronze.init import bronze_init
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 logger = logging.getLogger(__name__)
@@ -67,3 +65,64 @@ def ingest_orchestrator(context: df.DurableOrchestrationContext):
    
     return orchestrator_result_dict
 
+
+@app.activity_trigger(input_name="input")
+async def run_ingestion_activity(input: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Activity function to execute the bronze layer data ingestion logic AND
+    publish an event to Event Grid.
+    """
+    config = ConfigManager()
+    
+    input_payload = input["input_payload"]
+
+    correlation_id = input_payload.get("correlation_id", str(uuid.uuid4()))
+    logger.info(f"Running ingestion for correlation ID: {correlation_id}")
+    
+    result = None
+    try:
+        orchestrator = OrchestratorFactory.get_instance(ETLLayer.BRONZE, config=config)
+        parser = BronzeParser(config)
+        
+        bronze_context = parser.parse(input_payload)
+        bronze_context.correlation_id = correlation_id
+        
+        result = await orchestrator.execute(bronze_context)
+        return result.to_dict()
+
+    except Exception as e:
+        logger.exception(f"BronzeOrchestrator failed for correlation ID {correlation_id}.")
+        result_dict = {
+            "status": "FAILED",
+            "correlation_id": correlation_id,
+            "message": str(e),
+            "error_details": traceback.format_exc(),
+        }
+        return result_dict
+    
+    finally:
+        try:
+            notifier = EventGridNotifier(config.get("EVENT_GRID_ENDPOINT"), config.get("EVENT_GRID_KEY"))
+            silver_manifest_path = f"/silver/manifest/{input_payload.get("env")}.manifest.json"
+            event_grid_payload = {
+                "layer": ETLLayer.BRONZE.value,
+                "env": input_payload.get("env"),
+                "status": result.status,
+                "message_date": datetime.datetime.utcnow,
+                "correlation_id": result.correlation_id,
+                "manifest": silver_manifest_path,
+                "summary_ingestion_uri": result.summary_url,
+                "duration_in_ms": result.duration_in_ms
+            }
+            return notifier.send_notification(ETLLayer.BRONZE.value, "BronzeIngestionCompleted", event_grid_payload, result.correlation_id)
+            
+        except Exception as e:
+            logger.exception(f"Failed to send Event Grid notification: {e}")
+
+            result_dict = {
+                        "status": "FAILED",
+                        "correlation_id": correlation_id,
+                        "message": str(e),
+                        "error_details": traceback.format_exc(),
+                    }
+            return result_dict
